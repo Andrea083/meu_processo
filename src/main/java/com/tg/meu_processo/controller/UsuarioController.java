ab248db06c7135eb0969180523f9f84a96c00323
Andrea083 <andreab-lima@hotmail.com>
2026-04-24 19:37:51 -0300

    Preparo para integração com front e Adiciona configuração do Swagger

--- src/main/java/com/tg/meu_processo/controller/UsuarioController.java
+++ src/main/java/com/tg/meu_processo/controller/UsuarioController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //permite requisições da porta 3000, usada pelo front
 @RequestMapping("/api/usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
